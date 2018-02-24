261598b7905a655259fc8c4b31f60b103f673535
dmitry GA <dmitry@generalarcade.com>
2018-02-24 11:43:05 +0400

    initialize commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+    public float tumble;
+    //Rigidbody rigidbody;
+    // Use this for initialization
+    void Start () {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // transform.rotation = Random.rotation*0.1f;
+
+        //transform.eulerAngles = euler;
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+
+public class Test : MonoBehaviour {
+
+    private string logMesageGUI = "Debug Log:\n";
+    private void Update()
+    {
+        
+    }
+    private void Log(string message, string value = "")
+    {
+        Console.WriteLine(message + value);
+        logMesageGUI = logMesageGUI + "\n" + message + value;
+    }
+
+    private void OnGUI()
+    {
+        int screenWidth = Screen.width;
+        int screenHeight = Screen.height;
+        int rectWidth = screenWidth / 2;
+        int indent = 10;
+        GUIStyle style = new GUIStyle(GUI.skin.box);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = 20;
+        GUI.color = Color.white;
+        GUI.Box(new Rect(rectWidth, indent, rectWidth - indent, screenHeight - indent - indent), logMesageGUI, style);
+    }
+}
